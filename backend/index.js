04bbfa774de917f66cb3fdeaf9ac796bbd78dac4
not-lucky <57046712+not-lucky@users.noreply.github.com>
2024-07-07 13:32:26 +0530

    Exercises 3.13-3.14

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 
 const MAX = 100000000000
@@ -21,35 +23,18 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 // app.use(requestLogger)
 
 
-let phonebook = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 // app.get('/', (request, response) => {
 //   response.send('<h1>try a valid endpoint maybe?</h1>')
 // })
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebook)
+  console.log(`called`)
+  Person.find({}).then(res => {
+    // console.log('res', res)
+    response.json(res)
+  }
+  )
+
 })
 
 app.get('/info', (request, response) => {
@@ -94,22 +79,31 @@ app.post('/api/persons', (request, response) => {
     return
   }
 
-  if (phonebook.find(person => person.name === body.name)) {
-    response.status(409).json({
-      error: 'name must be unique'
-    })
-    return
-  }
+  // if (Person.find(person => person.name === body.name)) {
+  //   response.status(409).json({
+  //     error: 'name must be unique'
+  //   })
+  //   return
+  // }
 
-  const person = {
+  // const person = {
+  //   id: generateId(),
+  //   name: body.name,
+  //   number: body.number,
+  // }
+
+  const person = new Person({
     id: generateId(),
     name: body.name,
     number: body.number,
-  }
+  })
 
-  phonebook = phonebook.concat(person)
-
-  response.json(person)
+  person
+    .save()
+    .then((res) => {
+      console.log(`${res.name} saved successfullt`)
+      response.json(res)
+    })
 })
 
 
@@ -121,7 +115,7 @@ app.use(unknownEndpoint)
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
